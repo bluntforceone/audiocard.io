b959ccf570957d708b87af7f6715adf2c38b8964
Conrad Jones <cjones@turboit.co.uk>
2018-04-18 22:23:40 +0100

    CoreAudio skeleton

---
+++ include/osx/aio_coreaudio.h
@@ -0,0 +1,39 @@
+/****************************************************************************************************
+ * Copyright 2018 Conrad Jones                                                                      *
+ *                                                                                                  *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software    *
+ * and associated documentation files (the "Software"), to deal in the Software without             *
+ * restriction, including without limitation the rights to use, copy, modify, merge, publish,       *
+ * distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the    *
+ * Software is furnished to do so, subject to the following conditions:                             *
+ *                                                                                                  *
+ * The above copyright notice and this permission notice shall be included in all copies or         *
+ * substantial portions of the Software.                                                            *
+ *                                                                                                  *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING    *
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND       *
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,     *
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,   *
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.          *
+ *                                                                                                  *
+ ***************************************************************************************************/
+
+#ifndef AUDIOCARD_IO_COREAUDIO_H
+#define AUDIOCARD_IO_COREAUDIO_H
+
+#include "aio_audio.h"
+
+namespace acio {
+
+class CoreAudio : public Audio {
+public:
+    ~CoreAudio() override = default;
+
+public:
+    int countDevices() override;
+    DeviceInfo getDeviceInfo(int index) override;
+
+};
+}
+
+#endif //AUDIOCARD_IO_COREAUDIO_H
